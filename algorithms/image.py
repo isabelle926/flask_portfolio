afd41f45586f440e61e9a85223970f63ecb90736
isabelle <gunawanisabelle@gmail.com>
2021-09-17 13:37:26 -0700

    edited rgb.html and sign_up.html

--- algorithms/image.py
+++ algorithms/image.py
@@ -17,7 +17,7 @@ def image_formatter(img, img_type):
 
 
 # color_data prepares a series of images for data analysis
-def image_data(path="static/img/", img_list=None):  # path of static images is defaulted
+def image_data(path="static/assets/images/", img_list=None):  # path of static images is defaulted
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
             {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
@@ -68,7 +68,7 @@ def image_data(path="static/img/", img_list=None):  # path of static images is d
 
 # run this as standalone tester to see data printed in terminal
 if __name__ == "__main__":
-    local_path = "../static/img/"
+    local_path = "../static/assets/images/"
     img_test = [
         {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.png"},
     ]
